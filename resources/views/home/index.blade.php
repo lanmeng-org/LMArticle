6138e8a9845b5cc03c7832affbd1e922fd0027e4
Ming <mm@bigecko.com>
2017-08-04 17:56:09 +0800

    首页

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
   <div class="row">
+
     <div class="col-md-12">
       <div class="panel panel-primary">
         <div class="panel-heading">公告</div>
@@ -12,7 +13,6 @@
         </div>
       </div>
     </div>
-
 
     <div class="col-md-12">
       <div class="panel panel-default">
@@ -26,76 +26,6 @@
           @foreach(\App\Repositories\ArticleRepo::getList(5) as $item)
             <a href="{{ url('') }}" class="list-group-item">{{ $item->title }}</a>
           @endforeach
-        </div>
-      </div>
-    </div>
-    <div class="col-md-12">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          篮球下载
-          <div class="pull-right">
-            <a href="">查看更多</a>
-          </div>
-        </div>
-        <div class="panel-body">
-          Panel content
-        </div>
-      </div>
-    </div>
-
-
-    <div class="col-md-12">
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          足球节目
-          <div class="pull-right">
-            <a href="">查看更多</a>
-          </div>
-        </div>
-        <div class="panel-body">
-          Panel content
-        </div>
-      </div>
-    </div>
-    <div class="col-md-12">
-      <div class="panel panel-default">
-        <div class="panel-heading">篮球节目</div>
-        <div class="panel-body">
-          Panel content
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">足球视频</div>
-        <div class="panel-body">
-          Panel content
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">篮球视频</div>
-        <div class="panel-body">
-          Panel content
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">足球新闻</div>
-        <div class="panel-body">
-          Panel content
-        </div>
-      </div>
-    </div>
-    <div class="col-md-6">
-      <div class="panel panel-default">
-        <div class="panel-heading">篮球新闻</div>
-        <div class="panel-body">
-          Panel content
         </div>
       </div>
     </div>
