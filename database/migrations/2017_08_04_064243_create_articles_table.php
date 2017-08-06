37a1bec667cfb36664ea2d0856ce0a1c6a39a8e6
王明明 <lanmeng.org@gmail.com>
2017-08-06 17:29:26 +0800

    字段允许null

--- database/migrations/2017_08_04_064243_create_articles_table.php
+++ database/migrations/2017_08_04_064243_create_articles_table.php
@@ -18,11 +18,12 @@ class CreateArticlesTable extends Migration
             $table->string('title')->comment('文章标题');
             $table->integer('category_id')->comment('所属分类ID');
             $table->text('content')->comment('文章内容');
-            $table->string('keywords')->default('')->comment('文章关键词');
+            $table->string('keywords')->nullable()->comment('文章关键词');
             $table->integer('view_number')->default(0)->comment('浏览次数');
             $table->integer('position')->default(0)->comment('推荐位 1:文章页推荐 2:栏目页推荐 4:全站推荐');
-
             $table->timestamps();
+
+            $table->index('category_id');
         });
     }
 
