be6e89cd9e0d514d1fe0e582157167820e80353e
王明明 <lanmeng.org@gmail.com>
2017-08-06 14:49:46 +0800

    管理后台底部文字修改

--- resources/views/admin/layouts/_footer.blade.php
+++ resources/views/admin/layouts/_footer.blade.php
@@ -4,9 +4,6 @@
       <a href="#top">返回顶部</a>
     </div>
     <div>
-      {!! setting('footer_content') !!}
-    </div>
-    <div>
       本站 <a href="{{ url('/') }}" rel="nofollow">{{ setting('site_name') }}</a>
       由 <a href="http://www.lanmeng.org" target="_blank">蓝梦网络</a> 开发的文章发布系统搭建.
       基于 PHP / Bootstrap / Cerulean Theme 等技术.
