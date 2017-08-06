65cc0b7ee0cbc7316de26e2f99d5ff9149caf8d7
王明明 <lanmeng.org@gmail.com>
2017-08-06 15:00:57 +0800

    添加友情链接数据库

---
+++ database/migrations/2017_08_06_065624_create_links_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->comment('显示名称');
+            $table->string('link')->comment('链接地址');
+            $table->integer('order')->default(0)->comment('排序');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('links');
+    }
+}
